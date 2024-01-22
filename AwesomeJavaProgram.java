5046507b5f8057496499a1970423ba227cd72768
Angel Barajas <147221809+Ang-el12@users.noreply.github.com>
2024-01-21 20:39:16 -0800

    These Pretzels Are Making Me Thirsty!

--- AwesomeJavaProgram.java
+++ AwesomeJavaProgram.java
@@ -1,13 +1,24 @@
 public class AwesomeJavaProgram {
         public static void main(String[] args) {
-            int myInt = 7;
-            double shoeSize = 9.5;
-            char myInitial = 'J';
 
-            String myName = "John";
+          for (int i = 0; i < 10; i++) {
+              System.out.println("These pretzels are making me thirsty");
+          }
+        }
+        private static String printName(String name, int number) {
 
-            double result = myInt * shoeSize;
+            if (number != 5) {
+                System.out.println("This guy is awesome");
+            }
 
-            System.out.println(myName.toUpperCase());
+            else if (name.equals("Larry")) {
+                System.out.println("This guy is OK I guess");
+            }
+
+            else { System.out.println("I don't know this guy at all");
+            }
+
+
+            return "My name is " + name;
         }
     }
