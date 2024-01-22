2768dbc19d8f82b5be59eab48ffe565d48e91143
Angel Barajas <147221809+Ang-el12@users.noreply.github.com>
2024-01-21 20:16:22 -0800

    NamePrint - JOHN

---
+++ AwesomeJavaProgram.java
@@ -0,0 +1,13 @@
+public class AwesomeJavaProgram {
+        public static void main(String[] args) {
+            int myInt = 7;
+            double shoeSize = 9.5;
+            char myInitial = 'J';
+
+            String myName = "John";
+
+            double result = myInt * shoeSize;
+
+            System.out.println(myName.toUpperCase());
+        }
+    }
