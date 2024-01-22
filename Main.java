9ea75c6ba556bd4375153fc9c5cf6d9f456276c0
Angel Barajas <147221809+Ang-el12@users.noreply.github.com>
2024-01-21 19:30:42 -0800

    Add files via upload
    Hello World! 
    
    (2+4=?)

---
+++ Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello world!");
+
+       int num1 = 2;
+       int num2 = 4;
+       int sum = 0;
+
+       sum = num1 + num2;
+
+       System.out.println("\n\n The sum of " + num1 + " and "+ num2 + " is: " + sum + "\n\n");
+
+        }
+    }
